0599c37658acb2c1f14d870098297d377ca664f5
Ashis <achakraborty169@gmail.com>
2025-07-07 17:23:46 +0530

    assignment_2

--- Assignment_2/task2.py
+++ Assignment_2/task2.py
@@ -1,9 +1,9 @@
-# Step 1: Initialize a variable to store the sum
+
 total = 0
 
-# Step 2: Use a for loop to iterate from 1 to 50
-for number in range(1, 51):
-    total += number  # Add each number to the total
 
-# Step 3: Display the final sum
+for number in range(1, 51):
+    total += number  
+
+
 print("The sum of numbers from 1 to 50 is:", total)
